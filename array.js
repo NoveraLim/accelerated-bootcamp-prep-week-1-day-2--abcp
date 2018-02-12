328ac9cf794df4c41d037c092fb1f546cb2118cb
Novera Lim <lim.novera88@gmail.com>
2018-02-12 19:46:23 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
- 
+ return [...,]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
